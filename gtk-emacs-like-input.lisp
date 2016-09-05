45a96d8b9ce3b3473d1e0391698274fb0e99b5cb
stacksmith <fpgasm@apple2.x10.mx>
2016-09-05 16:23:08 -0700

    working use-entry, instant keys return a value to continue processing

--- gtk-emacs-like-input.lisp
+++ gtk-emacs-like-input.lisp
@@ -37,7 +37,7 @@
   (defun render (&key (match nil))
     (let ((keystr(buffer->string buffer) ))
       (unless match
-	(unless (zerop keystr)
+	(unless (zerop (length keystr))
 	  (setf match (keymap-match *keymap-top* keystr))))
       (gtk-label-set-text left keystr)
       (gtk-label-set-text middle "")
@@ -47,28 +47,45 @@
   
   (defun reset (&key (full nil))
     "reset input state and visuals."
+    (format t "RESET~%")
     (setf buffer (make-array 32 :fill-pointer 0 :adjustable t)
 	  (gtk-entry-text entry) "")
-    (trace)
-
+    (gtk-widget-hide entry)
+    
     (gtk-label-set-text left "")
     (gtk-label-set-text middle "")
     (gtk-label-set-text right "")
+
     (when full ; reset interactive stuff
       (and interactive
 	   (funcall interactive 2))
 	(setf interactive nil))
     t)
-  
+
+  (defun use-entry (on)
+    (format t "USE-ENTRY~%")
+    (if on
+	(progn
+	  (gtk-widget-hide middle)
+	  (gtk-widget-show entry)
+	  (gtk-widget-grab-focus entry))
+	(progn
+	  (gtk-widget-hide entry)
+	  (gtk-widget-show middle))))
 
 ;;; Instant commands
   (defun cmd-cancel ()
     (reset :full t))
 
   (defun cmd-back-up ()
-    (unless (zerop (length buffer))
-      (vector-pop buffer)
-      (render)))
+    (if interactive
+	nil ;let interactive do its own backup
+	(progn
+	  (unless (zerop (length buffer))
+	    (vector-pop buffer)
+	    (render)
+	    t ;we eat the key
+	    ))))
   
   (defun dispatch-match (match)
     (typecase match
@@ -76,28 +93,21 @@
        (funcall match) ;non-interactive function or lambda
        (reset))
       (symbol
-       (if (get match 'interactive)
-	   (progn ;install interactive and initialize it
-	     (funcall (setf interactive (symbol-function match)) 0))
-	   (progn ;call function and reset
-	     (funcall (symbol-function match))
-	     (reset)))))
-    )
+       (funcall (setf interactive (symbol-function match)) 0))))
   (defun input-keystroke ()
     "process a keystroke."
       (let ((match (keymap-instant-match *keymap-instant* (key->string key))))
-	(if match
-	    (funcall (symbol-function match)) ;
-	    (if interactive
-		(funcall interactive 1) ;returns nil/t to process keys in gtk
-		(progn
-		  (vector-push-extend key buffer) ;append key
-		  (let ((match (keymap-match *keymap-top* (buffer->string buffer))))
-		    (render :match match)
-		    (unless (listp match) (dispatch-match match)))
+	(unless (if match (funcall match)) ;instant processing nil? continue
+	  (if interactive
+	      (funcall interactive 1) ;returns nil/t to process keys in gtk
+	      (progn
+		(vector-push-extend key buffer) ;append key
+		(let ((match (keymap-match *keymap-top* (buffer->string buffer))))
+		  (render :match match)
+		  (unless (listp match) (dispatch-match match)))
 		t)))))
   
-    (defun on-key-press (widget event)
+  (defun on-key-press (widget event)
     "Process a key from GTK; return key structure or nil for special keys"
     (declare (ignore widget))
     (let ((gtkkey (gdk-event-key-keyval event)))
@@ -106,18 +116,22 @@
       (or (modifier-p gtkkey) ;do not process modifiers, gtk will handle them
 	  (input-keystroke) ;let them decide if to continue with key process
 	  )))
+  
+  (defun make-bar ()
+    (setf bar (make-instance 'gtk-box :orientation :horizontal )
+	  left (make-instance 'gtk-label :label "left"  )
+	  middle (make-instance 'gtk-label :label "middle" )
+	  entry (make-instance 'gtk-entry :label "entry"  )
+	  right (make-instance 'gtk-label :label "right" ))
     
-    (defun make-bar ()
-      (setf bar (make-instance 'gtk-box :orientation :horizontal )
-	    left (make-instance 'gtk-label :label "left"  )
-	    middle (make-instance 'gtk-label :label "middle" )
-	    entry (make-instance 'gtk-entry :label "entry" :hidden t )
-	    right (make-instance 'gtk-label :label "right" ))
-      (gtk-box-pack-start    bar left :expand nil)
-      (gtk-box-pack-start    bar middle :expand t)
-      (gtk-box-pack-start    bar right :expand nil)
-      (reset :full t)
-      bar))
+    (gtk-box-pack-start    bar left :expand nil)
+    (gtk-box-pack-start    bar middle :expand t)
+    (gtk-box-pack-start    bar entry :expand t)
+    
+    (gtk-box-pack-start    bar right :expand nil)
+    (reset :full t)
+    
+    bar))
 
 (defparameter *window* nil)
 (defun app-quit ()
@@ -130,21 +144,23 @@
 (defun fun2 () (format t "fun2") )
 (defun fun3 (stage)
   (case stage
-    (0 (format t "fun3: 0") t)
+    (0 (format t "fun3: 0")
+       (use-entry t) 1)
     (1 (format t "fun3: 1") nil)
-    (2 (format t "fun3: 2") t))
-)
-(setf (get 'fun3 'interactive) t)
+    (2 (format t "fun3: 2")
+       (use-entry nil) t))
+  )
+
 (defun bind-keys ()
   (setf *keymap-top* (new-keymap))
-  (bind *keymap-top* "C-xC-c" 'app-quit)
-  (bind *keymap-top* "C-a" 'fun1)
-  (bind *keymap-top* "C-b" 'fun2)
+  (bind *keymap-top* "C-xC-c" #'app-quit)
+  (bind *keymap-top* "C-a" #'fun1)
+  (bind *keymap-top* "C-b" #'fun2)
   (bind *keymap-top* "C-c" 'fun3)
 
   (setf *keymap-instant* (new-keymap))
-  (bind *keymap-instant* "C-g" 'cmd-cancel)
-  (bind *keymap-instant* "BS" 'cmd-back-up)
+  (bind *keymap-instant* "C-g" #'cmd-cancel)
+  (bind *keymap-instant* "BS" #'cmd-back-up)
   )
 (defun  test (&key (stdout *standard-output*))
   (within-main-loop
@@ -173,7 +189,8 @@
 			  (format t "done")
 			  (leave-gtk-main)))
       
-      (gtk-widget-show-all *window*)))
+      (gtk-widget-show-all *window*)
+      (reset)))
 
 
 
